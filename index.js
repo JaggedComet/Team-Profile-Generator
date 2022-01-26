64fb2bba004434cc5ec2bf48b922708dd28f3b6d
Marco Flores <marcoflores1029@gmail.com>
2022-01-25 22:42:43 -0800

    finished all the lib files, added an index and a prompt.js

--- index.js
+++ index.js
@@ -1,46 +1,8 @@
-// Import classes that are needed for the program
-const fs = require("fs");
-const inquirer = require("inquirer");
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+// // Import classes that are needed for the program
+// const fs = require("fs");
+// const inquirer = require("inquirer");
+// const Employee = require("./lib/Employee");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
 
-// Inquire prompt for the questions asked
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "";
-            name: "";
-            default: "";
-        },
-        {
-            type: "input",
-            message: "";
-            name: "";
-            default: "";
-        },
-        {
-            type: "input",
-            message: "";
-            name: "";
-            default: "";
-        },
-        {
-            type: "input",
-            message: "";
-            name: "";
-            default: "";
-        },
-        {
-            type: "input",
-            message: "";
-            name: "";
-            default: "";
-        },
-    ])
-
-    .then(response => {
-
-    });
